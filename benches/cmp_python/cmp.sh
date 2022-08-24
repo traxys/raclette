9dbc0d4d7536da43db9ed4f94a6417cb999bbffe
traxys <quentin+dev@familleboyer.net>
2022-08-24 19:39:58 +0200

    Implement hyperfine benchmarks

---
+++ benches/cmp_python/cmp.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+mkdir -p md
+mkdir -p json
+
+for bench in *.bench; do
+	printf "%s# %s%s\n" \
+		"$(tput bold)" \
+		"$(basename "$bench" .bench)" \
+		"$(tput sgr0)"
+	printf "  - python: %s%s%s\n" \
+		"$(tput sitm)" \
+		"$(head -n1 "$bench")" \
+		"$(tput sgr0)"
+	printf "  - raclette: %s%s%s\n" \
+		"$(tput sitm)" \
+		"$(tail -n1 "$bench")" \
+		"$(tput sgr0)"
+
+	hyperfine \
+		--export-markdown "md/$(basename "$bench" .bench).md" \
+		--export-json "json/$(basename "$bench" .bench).json" \
+		--warmup 1 \
+		--shell=none \
+		-n python "python -c 'print($(head -n1 "$bench"))'" \
+		-n raclette "../../target/release/raclette -c '$(tail -n1 "$bench")'"
+done
--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.naersk.url = "github:nix-community/naersk";
+  inputs.hyperfine = {
+    url = "github:sharkdp/hyperfine";
+    flake = false;
+  };
 
   outputs = {
     self,
@@ -11,6 +15,7 @@
     flake-utils,
     rust-overlay,
     naersk,
+    hyperfine,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
@@ -26,14 +31,37 @@
         cargo = rust;
         rustc = rust;
       };
+
+      hf-scripts = pkgs.stdenv.mkDerivation {
+        pname = "hyperfine-scripts";
+        version = "1.14.0";
+
+        src = "${hyperfine}/scripts";
+
+        installPhase = ''
+          mkdir -p $out/bin
+
+          for script in *.py; do
+          	cp "$script" $out/bin/hf-$(basename "$script" .py)
+          done
+        '';
+
+        propagatedBuildInputs = [
+          pkgs.python3Packages.numpy
+          pkgs.python3Packages.matplotlib
+          pkgs.python3Packages.scipy
+        ];
+      };
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
           pkgs.bashInteractive
           pkgs.cargo-fuzz
           pkgs.rustup
-		  pkgs.cargo-flamegraph
-		  pkgs.cargo-criterion
+          pkgs.cargo-flamegraph
+          pkgs.cargo-criterion
+          pkgs.hyperfine
+          hf-scripts
           rust
         ];
         buildInputs = [];
