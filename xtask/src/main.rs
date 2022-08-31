45cc30fe7b1e7d097499cfe2a60cb5384dc5dc7e
traxys <quentin+dev@familleboyer.net>
2022-08-31 23:25:38 +0200

    Use test framework

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,8 +1,7 @@
-use std::{fs, path::Path};
+use std::path::Path;
 
 use clap::{CommandFactory, Parser};
 use duct::cmd;
-use yansi::Paint;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -12,7 +11,6 @@ struct Args {
 
 #[derive(Parser, Debug)]
 enum Commands {
-    Test,
     Benches,
 }
 
@@ -36,48 +34,6 @@ fn main() -> color_eyre::Result<()> {
             cmd!(workspace_root.join("benches/cmp_python/cmp.sh"))
                 .dir(workspace_root.join("benches/cmp_python"))
                 .run()?;
-        }
-        Some(Commands::Test) => {
-            let expr_dir = workspace_root.join("tests/expr");
-
-            for entry in fs::read_dir(expr_dir)? {
-                let entry = entry?;
-                let file = std::fs::read_to_string(entry.path())?;
-                let newline = file
-                    .find('\n')
-                    .unwrap_or_else(|| panic!("no newline in test {:?}", entry.file_name()));
-                let expr = file[..newline].trim();
-                let result = file[newline + 1..].trim();
-                print!(
-                    "  {} ... ",
-                    Paint::new(entry.file_name().to_string_lossy()).bold()
-                );
-                let test_result = cmd!(env!("CARGO"), "run", "--", "-c", expr)
-                    .stdout_capture()
-                    .stderr_null()
-                    .unchecked()
-                    .run()?;
-                if !test_result.status.success() {
-                    println!(
-                        "{} (exit code = {})",
-                        Paint::red("not ok"),
-                        test_result.status
-                    );
-                } else {
-                    let test_result = String::from_utf8(test_result.stdout)?;
-                    let test_result = test_result.trim();
-                    if test_result != result {
-                        println!(
-                            "{} (expected = `{}`, got = `{}`)",
-                            Paint::red("not ok"),
-                            result,
-                            test_result,
-                        );
-                    } else {
-                        println!("{}", Paint::green("ok"),);
-                    }
-                }
-            }
         }
     }
 
