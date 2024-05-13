3b2a9fffec27c92e8216a73ff52b44e1194fc892
traxys <quentin+dev@familleboyer.net>
2024-05-13 23:04:39 +0200

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -9,68 +9,78 @@
     flake = false;
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-    naersk,
-    hyperfine,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      naersk,
+      hyperfine,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
 
-        overlays = [(import rust-overlay)];
-      };
-      rust = pkgs.rust-bin.stable.latest.default;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust = pkgs.rust-bin.stable.latest.default;
 
-      naersk' = pkgs.callPackage naersk {
-        cargo = rust;
-        rustc = rust;
-      };
+        naersk' = pkgs.callPackage naersk {
+          cargo = rust;
+          rustc = rust;
+        };
 
-      hf-scripts = pkgs.stdenv.mkDerivation {
-        pname = "hyperfine-scripts";
-        version = "1.14.0";
+        hf-scripts = pkgs.stdenv.mkDerivation {
+          pname = "hyperfine-scripts";
+          version = "1.14.0";
 
-        src = "${hyperfine}/scripts";
+          src = "${hyperfine}/scripts";
 
-        installPhase = ''
-          mkdir -p $out/bin
+          installPhase = ''
+            mkdir -p $out/bin
 
-          for script in *.py; do
-          	cp "$script" $out/bin/hf-$(basename "$script" .py)
-          done
-        '';
+            for script in *.py; do
+            	cp "$script" $out/bin/hf-$(basename "$script" .py)
+            done
+          '';
 
-        propagatedBuildInputs = [
-          pkgs.python3Packages.numpy
-          pkgs.python3Packages.matplotlib
-          pkgs.python3Packages.scipy
-        ];
-      };
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.bashInteractive
-          pkgs.cargo-fuzz
-          pkgs.cargo-flamegraph
-          pkgs.cargo-criterion
-          pkgs.cargo-tarpaulin
-          pkgs.hyperfine
-          pkgs.glow
-          hf-scripts
-          rust
-          pkgs.m4
-          (pkgs.python3.withPackages (ps: with ps; [toml pyyaml]))
-        ];
-        buildInputs = [];
-      };
+          propagatedBuildInputs = [
+            pkgs.python3Packages.numpy
+            pkgs.python3Packages.matplotlib
+            pkgs.python3Packages.scipy
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.bashInteractive
+            pkgs.cargo-fuzz
+            pkgs.cargo-flamegraph
+            pkgs.cargo-criterion
+            pkgs.cargo-tarpaulin
+            pkgs.hyperfine
+            pkgs.glow
+            hf-scripts
+            rust
+            pkgs.m4
+            (pkgs.python3.withPackages (
+              ps: with ps; [
+                toml
+                pyyaml
+              ]
+            ))
+          ];
+          buildInputs = [ ];
+        };
 
-      defaultPackage = naersk'.buildPackage {
-        src = ./.;
-        nativeBuildInputs = [pkgs.m4];
-      };
-    });
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = [ pkgs.m4 ];
+        };
+      }
+    );
 }
